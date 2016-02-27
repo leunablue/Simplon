5bafbe0806470d869e784b0eb3cf5cafc20139b0
Leùna <leunablue@users.noreply.github.com>
2016-02-27 14:18:55 +0100

    Added files via upload

---
+++ New David Bowie iOS App/New_DavidBowieApp/DavidBowieApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DavidBowieApp
+//
+//  Created by Leuna O'Doherty on 23/02/2016.
+//  Copyright © 2016 Leuna O'Doherty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
